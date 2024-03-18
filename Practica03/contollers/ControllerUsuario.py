436ca4599e0f9b2a21546c8cc4e5139678b7d0f7
MarcosNor <noriegamarcos44@ciencias.unam.mx>
2024-03-17 19:43:31 -0600

    Agregar Usuario completo y cambios a algunos html

--- Practica03/contollers/ControllerUsuario.py
+++ Practica03/contollers/ControllerUsuario.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
-from alchemyClasses.Usuario import Usuario
+from sqlalchemy.exc import IntegrityError
+
+from alchemyClasses.Usuario import Usuario, db
 from model.Model_usuario import borra_usuario
 
 usuario_blueprint = Blueprint('usuario', __name__, url_prefix='/usuario')
@@ -13,7 +15,6 @@ def ver_usuarios():
     usuarios = Usuario.query.all()
     for usuario in usuarios:
         print(f"ID: {usuario.idUsuario}, Nombre: {usuario.nombre}, Email: {usuario.email}")
-    #usuarios = Usuario.query.all()
     return render_template('Usuario/ver_todos.html', usuarios=usuarios)
 
 @usuario_blueprint.route('/id/<int:id_usuario>/<string:nombre>')
@@ -26,22 +27,31 @@ def ver_usuario_id(id_usuario, nombre):
 
 @usuario_blueprint.route('/agregar', methods=['GET', 'POST'])
 def agregar_usuario():
-    if request.method == 'GET':
-        return render_template('Usuario/add_user.html')
-    else:
-        nombre = request.form['name']
-        password = request.form['password']
-        email = request.form['email']
-        # Puedes manejar otros campos como la imagen de perfil y si es superusuario aquí
-        nuevo_usuario = Usuario(nombre=nombre, password=password, email=email)
-        db.session.add(nuevo_usuario)
-        db.session.commit()
-        flash('Usuario agregado correctamente', 'success')
-        return "Usuario agregado"
-        #return redirect(url_for('usuario.ver_usuarios'))
+    if request.method == 'POST':
+        nombree = request.form['nombre']
+        ap_pat = request.form['apPat']
+        ap_mat = request.form['apMat']
+        passwd = request.form['password']
+        emaill = request.form['email']
+        #profilePictureU = request.form['profilePicture']
+        #superUserU = request.form['superUser']
+
+        nuevo_usuario = Usuario(nombre=nombree, apPat=ap_pat, apMat=ap_mat, password=passwd, email=emaill,
+                                profilePicture=None, superUser=0)
+        try:
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+            flash('Usuario agregado correctamente', 'success')
+            return redirect(url_for('usuario.agregar_usuario'))
+        except IntegrityError:
+            db.session.rollback()  # Revertir los cambios realizados en la transacción
+            flash('Error: El correo electrónico ya está registrado', 'error')
+            return redirect(url_for('usuario.agregar_usuario'))
+
+    return render_template('Usuario/add_user.html')
+
 
 @usuario_blueprint.route('/borrar',  methods=['GET', 'POST'])
 def borrar_usuario():
     #borra_usuario(1)
-    #print("Borrado con éxito!")
-    return "borrar"
+    return "ola"
